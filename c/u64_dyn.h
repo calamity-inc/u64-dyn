e50b7bfa49799dd01e65628deb141441b9493fa8
Sainan <63328889+Sainan@users.noreply.github.com>
2025-08-10 21:06:31 +0200

    Add i64_dyn v1 encoding to C implementation (#4)

--- c/u64_dyn.h
+++ c/u64_dyn.h
@@ -8,6 +8,8 @@ size_t pack_u64_dyn(uint8_t out[9], uint64_t v);
 uint64_t unpack_u64_dyn(const uint8_t* in_data, size_t in_size, size_t* out_size);
 size_t pack_u64_dyn_v2(uint8_t out[9], uint64_t v);
 uint64_t unpack_u64_dyn_v2(const uint8_t* in_data, size_t in_size, size_t* out_size);
+size_t pack_i64_dyn(uint8_t out[9], int64_t v);
+int64_t unpack_i64_dyn(const uint8_t* in_data, size_t in_size, size_t* out_size);
 size_t pack_i64_dyn_v2(uint8_t out[9], int64_t v);
 int64_t unpack_i64_dyn_v2(const uint8_t* in_data, size_t in_size, size_t* out_size);
 
