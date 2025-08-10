12e7bcd14c9cf58f1e6d062c8f0f135a21a12d0d
Sainan <63328889+Sainan@users.noreply.github.com>
2025-08-10 20:31:12 +0200

    Align C unpack_u64_dyn_v2 with Lua behavior (#1)

--- c/u64_dyn.c
+++ c/u64_dyn.c
@@ -2,57 +2,56 @@
 
 size_t pack_u64_dyn_v2(uint8_t out[9], uint64_t v)
 {
-	size_t i = 0;
-	while (i != 8)
-	{
-		const uint8_t cur = v & 0x7f;
-		v >>= 7;
-		if (v != 0)
-		{
-			out[i++] = cur | 0x80;
-			v -= 1; // v2
-		}
-		else
-		{
-			out[i++] = cur;
-			return i;
-		}
-	}
-	if (v != 0)
-	{
-		out[i++] = v;
-	}
-	return i;
+    size_t i = 0;
+    while (i != 8)
+    {
+        const uint8_t cur = v & 0x7f;
+        v >>= 7;
+        if (v != 0)
+        {
+            out[i++] = cur | 0x80;
+            v -= 1; // v2
+        }
+        else
+        {
+            out[i++] = cur;
+            return i;
+        }
+    }
+    if (v != 0)
+    {
+        out[i++] = v;
+    }
+    return i;
 }
 
 uint64_t unpack_u64_dyn_v2(const uint8_t* in_data, size_t in_size, size_t* out_size)
 {
-	uint64_t v = 0;
-	int bits = 0;
-	if (in_size)
-	{
-		uint8_t b = *in_data;
-		v = b & 0x7f;
-		int has_next = b >> 7;
-		bits += 7;
-		while (has_next && (++in_data, --in_size))
-		{
-			b = *in_data;
-			has_next = 0;
-			if (bits < 56)
-			{
-				has_next = *in_data >> 7;
-				b &= 0x7f;
-			}
-			v = v | ((b + 1) << bits);
-			bits += 7;
-		}
-	}
-	if (out_size)
-	{
-		*out_size = bits / 7;
-	}
-	return v;
+    uint64_t v = 0;
+    int bits = 0;
+    size_t used = 0;
+    while (used < in_size && used < 8)
+    {
+        uint8_t b = in_data[used++];
+        v += (uint64_t)(b & 0x7f) << bits;
+        if ((b & 0x80) == 0)
+        {
+            goto done;
+        }
+        bits += 7;
+        v += (uint64_t)1 << bits;
+    }
+    if (used < in_size)
+    {
+        uint8_t b = in_data[used++];
+        v += (uint64_t)b << 56;
+    }
+  done:
+    if (out_size)
+    {
+        *out_size = used;
+    }
+    return v;
 }
 
 size_t pack_i64_dyn_v2(uint8_t out[9], int64_t v)
@@ -67,13 +66,13 @@ size_t pack_i64_dyn_v2(uint8_t out[9], int64_t v)
     {
         u = v;
     }
-	return pack_u64_dyn_v2(out, (neg << 6) | ((u & ~0x3f) << 1) | (u & 0x3f));
+    return pack_u64_dyn_v2(out, (neg << 6) | ((u & ~0x3f) << 1) | (u & 0x3f));
 }
 
 int64_t unpack_i64_dyn_v2(const uint8_t* in_data, size_t in_size, size_t* out_size)
 {
-	uint64_t u = unpack_u64_dyn_v2(in_data, in_size, out_size);
-	const int neg = (u >> 6) & 1; // check bit 6
+    uint64_t u = unpack_u64_dyn_v2(in_data, in_size, out_size);
+    const int neg = (u >> 6) & 1; // check bit 6
     u = ((u >> 1) & ~0x3f) | (u & 0x3f); // remove bit 6
     int64_t v;
     if (neg)
