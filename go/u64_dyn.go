ee1303d82f5c89943ebd3c08a3810401d9abd037
Baloo <73783549+41Baloo@users.noreply.github.com>
2025-08-11 19:44:16 +0000

    Minor performance improvements
    Replaced tabs with 4 spaces
    
    Removed unneeded size calculation
    
    Yeet tabs
    
    Refactored names
    
    Formatted code

--- go/u64_dyn.go
+++ go/u64_dyn.go
@@ -1,157 +1,205 @@
 package u64dyn
 
-import "errors"
+import (
+	"errors"
+)
+
+var (
+	ErrInsufficientData = errors.New("insufficient data")
+)
 
 // PackU64Dyn packs an integer into u64_dyn bytes.
-func PackU64Dyn(v uint64) []byte {
-	out := make([]byte, 0, 9)
-	for i := 0; i < 8; i++ {
-		cur := byte(v & 0x7f)
-		v >>= 7
-		if v != 0 {
-			out = append(out, cur|0x80)
-		} else {
-			out = append(out, cur)
-			return out
-		}
-	}
-	if v != 0 {
-		out = append(out, byte(v))
-	}
-	return out
+func PackU64Dyn(in uint64) []byte {
+	var buff [9]byte
+	return packV1(in, &buff)
 }
 
 // UnpackU64Dyn unpacks a u64_dyn encoded integer from buf starting at offset.
 // It returns the value, the new offset, or an error if there is insufficient data.
-func UnpackU64Dyn(buf []byte, offset int) (uint64, int, error) {
-	var v uint64
-	bits := uint(0)
-	used := 0
+func UnpackU64Dyn(buf []byte, offset int) (result uint64, off int, err error) {
+	var bits uint
+	var used int
+	bufLen := len(buf)
+
 	for used < 8 {
-		if offset+used >= len(buf) {
-			return 0, offset, errors.New("insufficient data")
+		if offset+used >= bufLen {
+			return 0, offset, ErrInsufficientData
 		}
 		b := uint64(buf[offset+used])
-		v += (b & 0x7f) << bits
+		result |= (b & 0x7f) << bits
 		used++
 		if (b & 0x80) == 0 {
-			return v, offset + used, nil
+			return result, offset + used, nil
 		}
 		bits += 7
 	}
-	if offset+used >= len(buf) {
-		return 0, offset, errors.New("insufficient data")
+	if offset+used >= bufLen {
+		return 0, offset, ErrInsufficientData
 	}
 	b := uint64(buf[offset+used])
-	v += b << 56
+	result |= b << 56
 	used++
-	return v, offset + used, nil
+	return result, offset + used, nil
 }
 
 // PackU64DynV2 packs an integer using u64_dyn_v2.
-func PackU64DynV2(v uint64) []byte {
-	out := make([]byte, 0, 9)
-	for i := 0; i < 8; i++ {
-		cur := byte(v & 0x7f)
-		v >>= 7
-		if v != 0 {
-			out = append(out, cur|0x80)
-			v-- // v2
-		} else {
-			out = append(out, cur)
-			return out
-		}
-	}
-	if v != 0 {
-		out = append(out, byte(v))
-	}
-	return out
+func PackU64DynV2(in uint64) []byte {
+	var buff [9]byte
+	return packV2(in, &buff)
 }
 
 // UnpackU64DynV2 unpacks a u64_dyn_v2 encoded integer from buf starting at offset.
 // It returns the value, the new offset, or an error if there is insufficient data.
-func UnpackU64DynV2(buf []byte, offset int) (uint64, int, error) {
-	var v uint64
-	bits := uint(0)
-	used := 0
+func UnpackU64DynV2(buf []byte, offset int) (result uint64, off int, err error) {
+	var bits uint
+	var used int
+	bufLen := len(buf)
+
 	for used < 8 {
-		if offset+used >= len(buf) {
-			return 0, offset, errors.New("insufficient data")
+		if offset+used >= bufLen {
+			return 0, offset, ErrInsufficientData
 		}
 		b := uint64(buf[offset+used])
-		v += (b & 0x7f) << bits
+		result += (b & 0x7f) << bits
 		used++
 		if (b & 0x80) == 0 {
-			return v, offset + used, nil
+			return result, offset + used, nil
 		}
 		bits += 7
-		v += 1 << bits // v2
+		result += 1 << bits
 	}
-	if offset+used >= len(buf) {
-		return 0, offset, errors.New("insufficient data")
+	if offset+used >= bufLen {
+		return 0, offset, ErrInsufficientData
 	}
 	b := uint64(buf[offset+used])
-	v += b << 56
+	result += b << 56
 	used++
-	return v, offset + used, nil
+	return result, offset + used, nil
 }
 
 // PackI64Dyn packs a signed integer using i64_dyn.
-func PackI64Dyn(v int64) []byte {
-	var u uint64 = uint64(v)
-	neg := uint64(0)
-	if v < 0 {
-		neg = 1
-		u = (^u + 1) & ^(uint64(1) << 63)
-	}
-	packed := (neg << 6) | ((u & ^uint64(0x3f)) << 1) | (u & 0x3f)
-	return PackU64Dyn(packed)
+func PackI64Dyn(in int64) []byte {
+	var buff [9]byte
+	return packI64V1(in, &buff)
 }
 
 // UnpackI64Dyn unpacks an i64_dyn encoded integer from buf starting at offset.
 // It returns the value, the new offset, or an error if there is insufficient data.
-func UnpackI64Dyn(buf []byte, offset int) (int64, int, error) {
-	u64, idx, err := UnpackU64Dyn(buf, offset)
+func UnpackI64Dyn(buf []byte, offset int) (result int64, off int, err error) {
+	u, off, err := UnpackU64Dyn(buf, offset)
 	if err != nil {
-		return 0, offset, err
+		return 0, off, err
 	}
-	neg := (u64 >> 6) & 1
-	u := ((u64 >> 1) & ^uint64(0x3f)) | (u64 & 0x3f)
-	var v int64 = int64(u)
-	if neg != 0 {
-		v = int64((^(u - 1)) | (uint64(1) << 63))
+
+	sign := (u >> 6) & 1
+	u63 := ((u >> 1) & ^uint64(0x3f)) | (u & 0x3f)
+
+	if sign == 0 {
+		return int64(u63), off, nil
 	}
-	return v, idx, nil
+
+	if u63 == 0 {
+		return -1 << 63, off, nil
+	}
+
+	return -int64(u63), off, nil
 }
 
 // PackI64DynV2 packs a signed integer using i64_dyn_v2.
-func PackI64DynV2(v int64) []byte {
-	var u uint64
-	var neg uint64
-	if v < 0 {
-		neg = 1
-		u = uint64(-v) - 1
-	} else {
-		u = uint64(v)
-	}
-	packed := (neg << 6) | ((u & ^uint64(0x3f)) << 1) | (u & 0x3f)
-	return PackU64DynV2(packed)
+func PackI64DynV2(in int64) []byte {
+	var buff [9]byte
+	return packI64V2(in, &buff)
 }
 
 // UnpackI64DynV2 unpacks an i64_dyn_v2 encoded integer from buf starting at offset.
 // It returns the value, the new offset, or an error if there is insufficient data.
-func UnpackI64DynV2(buf []byte, offset int) (int64, int, error) {
-	u64, idx, err := UnpackU64DynV2(buf, offset)
+func UnpackI64DynV2(buf []byte, offset int) (result int64, off int, err error) {
+	u, off, err := UnpackU64DynV2(buf, offset)
 	if err != nil {
-		return 0, offset, err
+		return 0, off, err
 	}
-	neg := (u64 >> 6) & 1
-	u := ((u64 >> 1) & ^uint64(0x3f)) | (u64 & 0x3f)
-	var v int64
-	if neg != 0 {
-		v = -int64(u) - 1
+
+	sign := (u >> 6) & 1
+	u63 := ((u >> 1) & ^uint64(0x3f)) | (u & 0x3f)
+
+	if sign == 0 {
+		return int64(u63), off, nil
+	}
+
+	return int64(^u63), off, nil
+}
+
+func packV1(in uint64, out *[9]byte) []byte {
+	for i := 0; i < 8; i++ {
+		cur := byte(in & 0x7f)
+		in >>= 7
+		if in != 0 {
+			out[i] = cur | 0x80
+		} else {
+			out[i] = cur
+			return out[:i+1]
+		}
+	}
+	if in != 0 {
+		out[8] = byte(in)
+	}
+
+	return out[:]
+}
+
+func packV2(in uint64, out *[9]byte) []byte {
+	for i := 0; i < 8; i++ {
+		cur := byte(in & 0x7f)
+		in >>= 7
+		if in != 0 {
+			out[i] = cur | 0x80
+			in--
+		} else {
+			out[i] = cur
+			return out[:i+1]
+		}
+	}
+	if in != 0 {
+		out[8] = byte(in)
+	}
+
+	return out[:]
+}
+
+func packI64V1(in int64, out *[9]byte) []byte {
+	neg := in < 0
+
+	var u63 uint64
+	if neg {
+		u63 = uint64(-in) & ^(uint64(1) << 63)
 	} else {
-		v = int64(u)
+		u63 = uint64(in)
 	}
-	return v, idx, nil
+
+	var sign uint64
+	if neg {
+		sign = 1
+	}
+
+	u := (sign << 6) | ((u63 & ^uint64(0x3f)) << 1) | (u63 & 0x3f)
+	return packV1(u, out)
+}
+
+func packI64V2(in int64, out *[9]byte) []byte {
+	neg := in < 0
+
+	var u63 uint64
+	if neg {
+		u63 = uint64(^in)
+	} else {
+		u63 = uint64(in)
+	}
+
+	var sign uint64
+	if neg {
+		sign = 1
+	}
+
+	u := (sign << 6) | ((u63 & ^uint64(0x3f)) << 1) | (u63 & 0x3f)
+	return packV2(u, out)
 }
