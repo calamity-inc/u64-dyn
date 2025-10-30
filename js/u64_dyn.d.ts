412dd141b8c8da600b1025618489b5daa90f6f75
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-30 10:49:00 +0100

    [JS] Update typings to reflect that input values can be numbers

--- js/u64_dyn.d.ts
+++ js/u64_dyn.d.ts
@@ -1,39 +1,39 @@
-export function pack_u64_dyn(value: bigint): Uint8Array;
+export function pack_u64_dyn(value: bigint | number): Uint8Array;
 export function unpack_u64_dyn(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
-export function pack_u64_dyn_b(value: bigint): Uint8Array;
+export function pack_u64_dyn_b(value: bigint | number): Uint8Array;
 export function unpack_u64_dyn_b(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
-export function pack_i64_dyn_a(value: bigint): Uint8Array;
+export function pack_i64_dyn_a(value: bigint | number): Uint8Array;
 export function unpack_i64_dyn_a(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
-export function pack_i64_dyn_b(value: bigint): Uint8Array;
+export function pack_i64_dyn_b(value: bigint | number): Uint8Array;
 export function unpack_i64_dyn_b(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
 /** @deprecated Renamed to pack_u64_dyn_b */
-export function pack_u64_dyn_v2(value: bigint): Uint8Array;
+export function pack_u64_dyn_v2(value: bigint | number): Uint8Array;
 /** @deprecated Renamed to unpack_u64_dyn_b */
 export function unpack_u64_dyn_v2(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
 /** @deprecated Renamed to pack_i64_dyn_a */
-export function pack_i64_dyn(value: bigint): Uint8Array;
+export function pack_i64_dyn(value: bigint | number): Uint8Array;
 /** @deprecated Renamed to unpack_i64_dyn_a */
 export function unpack_i64_dyn(
   data: Uint8Array,
   offset?: number,
 ): [bigint, number];
 /** @deprecated Renamed to pack_i64_dyn_b */
-export function pack_i64_dyn_v2(value: bigint): Uint8Array;
+export function pack_i64_dyn_v2(value: bigint | number): Uint8Array;
 /** @deprecated Renamed to unpack_i64_dyn_b */
 export function unpack_i64_dyn_v2(
   data: Uint8Array,
